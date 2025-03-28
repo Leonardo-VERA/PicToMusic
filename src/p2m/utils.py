98dba574c0b476497352cedaf274bbe414a8e1c7
Augustin Morval <augustin.morval.pro@gmail.com>
2025-03-28 10:49:46 +0100

    Add dataset extraction and batching utilities

--- src/p2m/utils.py
+++ src/p2m/utils.py
@@ -5,6 +5,11 @@ import csv
 from pathlib import Path
 from typing import List, Tuple
 from p2m.scoretyping import StaffLine
+import zipfile
+import os
+from tqdm import tqdm
+import loguru
+import shutil
 
 def imreshape(image: np.ndarray, shape: int = 128):
     return cv2.resize(image, (shape, shape))
@@ -17,6 +22,7 @@ def generate_detection_csv(staff_lines_list: List[List[StaffLine]],
     Args:
         staff_lines (List[StaffLine]): List of staff line lists, one for each score
         output_filename (str): Path to output CSV file
+        include_staff (bool): Whether to include staff lines in the CSV
     """
      
     output_path = Path(output_filename)
@@ -60,3 +66,50 @@ def generate_detection_csv(staff_lines_list: List[List[StaffLine]],
                         x + w,          # xmax
                         y + h           # ymax
                     ])
+                    
+
+def extract_dataset(zip_path: str):
+    
+    if not os.path.exists(zip_path):
+        raise FileNotFoundError(f"Could not find {zip_path}")
+        
+    with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+        loguru.logger.info(f"Extracting {zip_path}")
+        total_size = sum(file.file_size for file in zip_ref.filelist)
+        loguru.logger.info(f"Total size: {total_size} bytes")
+        
+        with tqdm(total=total_size, unit='B', unit_scale=True, desc="Extracting") as pbar:
+            for file in zip_ref.filelist:
+                zip_ref.extract(file)
+                pbar.update(file.file_size)
+    
+    loguru.logger.info(f"Successfully extracted {zip_path}")
+    
+def split_data(input_path: str, output_path: str, batch_size: int, num_batch: int):
+    """
+    Split the dataset into multiple batches and save them to the output path.
+    
+    Args:
+        input_path (str): Path to the input dataset
+        output_path (str): Path to save the batches
+        batch_size (int): Number of files per batch
+        num_batch (int): Number of batches to create
+    """
+
+    os.makedirs(output_path, exist_ok=True)
+    
+    files = os.listdir(input_path)
+    
+    for batch_num in tqdm(range(num_batch), desc="Processing batches", total=num_batch, unit="batch"):
+        batch_folder = os.path.join(output_path, f'batch_{batch_num}')
+        os.makedirs(batch_folder, exist_ok=True)
+        
+        start_idx = batch_num * batch_size
+        end_idx = min((batch_num + 1) * batch_size, len(files))
+        
+        for file_name in files[start_idx:end_idx]:
+            src = os.path.join(input_path, file_name)
+            dst = os.path.join(batch_folder, file_name)
+            shutil.copy2(src, dst)
+    loguru.logger.info(f"Successfully processed {num_batch} batches and saved to {output_path}")
+    return output_path
