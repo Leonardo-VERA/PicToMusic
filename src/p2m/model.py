052cf8c1d19b450fec5e05a216c8dc0a3a77e4cc
Augustin Morval <augustin.morval.pro@gmail.com>
2025-03-29 15:55:21 +0100

    added saving option

--- src/p2m/model.py
+++ src/p2m/model.py
@@ -5,7 +5,7 @@ def train(data_path: str, model_path: str = "yolo11n.pt", epochs: int = 200):
     model.train(data=data_path, epochs=epochs)
     return model
 
-def predict(model_path: str, image_path: str, save_path: str = None):
+def predict(model_path: str, image_path: str, save : bool = False, save_path: str = None):
     model = YOLO(model_path)
-    return model.predict(image_path, save=save_path)
+    return model.predict(image_path, save=save, project=save_path)
 
